d968a7ca4537caaa563d05177579562e38ccc6db
RinaDish <dishli271113@gmail.com>
2020-09-08 00:35:47 +0300

    added ajax requests to server

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-import { generateLog, addLog, endGame } from './utils.js';
+import { generateLog, addLog } from './utils.js';
 
 class Selectors {
     constructor(name) {
@@ -27,11 +27,12 @@ export default class Pokemon extends Selectors {
         const perc = this.hp.current * (100 / this.hp.total);
         this.elHp.innerText = this.hp.current + ' / ' + this.hp.total;
         this.elProgressbar.style.width = perc + '%';
+        if ( perc > 60) this.elProgressbar.setAttribute('class', 'health')
         if (perc < 60 && perc > 20) this.elProgressbar.setAttribute('class', 'health low')
         if (perc < 20 && perc > 0) this.elProgressbar.setAttribute('class', 'health critical')
     };
 
-    changeHP = (count) => {
+    changeHP = (count, player, enemy) => {
         this.hp.current -= count;
 
         if (this.hp.current <= count) this.hp.current = 0
@@ -39,10 +40,7 @@ export default class Pokemon extends Selectors {
 
         this.renderHP();
 
-        const log = generateLog(this, count);
+        const log = generateLog(this, count, player, enemy);
         addLog(log);
-
-
-        if (this.hp.current === 0) endGame(`${this.name} lost =<`);
     }
 };
